428c55a588455177fd0b7f38a26bd78b5552f825
Tommaso Tadiello <tommaso.attimi@pm.me>
2024-05-21 15:10:26 +0200

    created server side

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const eventRoutes = require("./routes/eventRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to MongoDB");
+});
+
+// Routes
+app.use("/api/events", eventRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
